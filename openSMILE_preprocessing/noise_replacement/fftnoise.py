f0ee5d3f13bf2b3a211b4db7d6270022314998da
Frank Zalkow <frank_zalkow@web.de>
2016-01-29 10:54:39 -0500

    port from matlab to python

--- openSMILE_preprocessing/noise_replacement/fftnoise.py
+++ openSMILE_preprocessing/noise_replacement/fftnoise.py
@@ -1,4 +1,4 @@
-if 0
+"""
     Copyright (c) 2011, Aslak Grinsted
     All rights reserved.
 
@@ -23,53 +23,21 @@ if 0
     CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
     ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
     POSSIBILITY OF SUCH DAMAGE.
-end
+"""
+import numpy as np
 
-function noise=fftnoise(f,Nseries)
-% Generate noise with a given power spectrum.
-% Useful helper function for Monte Carlo null-hypothesis tests and confidence interval estimation.
-%
-% noise=fftnoise(f[,Nseries])
-%
-% INPUTS:
-% f: the fft of a time series (must be a column vector)
-% Nseries: number of noise series to generate. (default=1)
-%
-% OUTPUT:
-% noise: surrogate series with same power spectrum as f. (each column is a surrogate).
-%
-% ------ Example: ------
-% %calculate if the trend is significantly different from zero
-% %(Null-hypothesis: a random process with the same power spectrum as data).
-%
-% x=(1:100)';
-% data=smooth(randn(size(x)),15);
-% pdata=polyfit(x,data,1)
-% f=fft(data);
-% psur=nan(length(pdata),10000);
-% for ii=1:size(psur,2)
-%   psur(:,ii)=polyfit(x,fftnoise(f),1)';
-% end
-% ptile=prctile(psur(1,:)',[2.5 97.5])
-% if (pdata(1)>ptile(2))|(pdata(1)<ptile(1))
-%   disp('significant trend')
-% else
-%   disp('not significant trend')
-% end
-%
-%   --- Aslak Grinsted (2009)
-if nargin<2
-    Nseries=1;
-end
+def fftnoise(f):
+    f = np.array(f, dtype='complex')
+    Np = (len(f) - 1) // 2
+    phases = np.random.rand(Np) * 2 * np.pi
+    phases = np.cos(phases) + 1j * np.sin(phases)
+    f[1:Np+1] *= phases
+    f[-1:-1-Np:-1] = np.conj(f[1:Np+1])
+    return np.fft.ifft(f).real
 
-f=f(:);
-N=length(f);
-Np=floor((N-1)/2);
-phases=rand(Np,Nseries)*2*pi;
-phases=complex(cos(phases),sin(phases)); % this was the fastest alternative in my tests.
-
-f=repmat(f,1,Nseries);
-f(2:Np+1,:)=f(2:Np+1,:).*phases;
-f(end:-1:end-Np+1,:)=conj(f(2:Np+1,:));
-
-noise=real(ifft(f,[],1));
+def band_limited_noise(min_freq, max_freq, samples=1024, samplerate=1):
+    freqs = np.abs(np.fft.fftfreq(samples, 1/samplerate))
+    f = np.zeros(samples)
+    idx = np.where(np.logical_and(freqs>=min_freq, freqs<=max_freq))[0]
+    f[idx] = 1
+    return fftnoise(f)
