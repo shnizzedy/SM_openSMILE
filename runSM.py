82314f600fbdaa79460cb27614ee146d0ae08cb2
Jon Clucas <jon@vasegurt.com>
2016-11-11 16:32:23 -0500

    modetype int

--- runSM.py
+++ runSM.py
@@ -66,7 +66,7 @@ def runSM():
 						# if not, create that folder
 						participant_home_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(proot,wav))))
 						if not os.path.exists(os.path.join(participant_home_dir,config_file.strip('.conf'))):
-							os.mkdir(os.path.join(participant_home_dir,config_file.strip('.conf')),'0755')
+							os.mkdir(os.path.join(participant_home_dir,config_file.strip('.conf')),0755)
 						# run openSMILE and send results to all_audio_files/[URSI]/[config]
 						row, table_path = ex.run_openSMILE(os.path.abspath(os.path.join(proot,wav)),'../SMILExtract',
 										   '-I','-C','-O',
