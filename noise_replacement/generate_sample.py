a0cf4d969c43db22d06109995e6b2b0a9f3eb50e
Jon Clucas <jon.clucas@childmind.org>
2016-12-21 19:47:14 -0500

    debug

--- noise_replacement/generate_sample.py
+++ noise_replacement/generate_sample.py
@@ -13,11 +13,18 @@ Created on Wed Dec 21 16:34:37 2016
 
 @author: jon.clucas
 """
-import iterate_ursis as iu, noise_replacement as nr, pydub, random
+# workaround for local installation : adjust these paths to local installation
+#     directories
+import sys
+sys.path.append("/Volumes/Jon.Clucas/openSMILE/SM_openSMILE")
+sys.path.append("/Library/Frameworks/Python.framework/Versions/2.7/lib/"
+                "python2.7/site-packages")
+
+import iterate_ursis as iu, noise_replacement as nr, os, pydub, random
 
 def create_sample(in_file):
-	"""
-	Function to iterate through [top_directory]/URSI/[subdirectory]/files
+    """
+    Function to iterate through [top_directory]/URSI/[subdirectory]/files
     structure and perform a function on each file.
 
     Parameters
@@ -28,31 +35,34 @@ def create_sample(in_file):
     Returns
     -------
     None
-	"""
-	out_file = os.path.join([os.path.dirname(os.path.dirname(in_file)), 
-	             "sample_silenced", os.path.basename(in_file)])
-	options = nr.get_ambient_clips(in_file)
-	remaining_options = []
-	for pair in options:
-		if ((pair[1] - pair[0]) > 110250):
-			remaining_options.append(pair)
-	num_options = len(remaining_options)
-	if num_options == 0:
-		remaing_options = options
-		num_options = len(remaining_options)
-	chosen_one = remaining_options[random.randrange(0, num_options)]
-	original = pydub.AudioSegment.from_wav(in_file)
-	sample_clip = original.get_sample_slice(0, chosen_one[0])
-	sample_clip.append(pydub.AudioSegment.silent(duration=(chosen_one[1] -
-	                   chosen_one[0])))
-	sample_clip.append(original.get_sample_slice(chosen_one[1], None)
-	print(''.join(["Exporting ", os.path.basename(os.path.dirname(outfile)), 
-	      "/", os.path.basename(out_file)]))
-	sample_clip.export(out_file, format="wav")
+    """
+    out_file = os.path.join([os.path.dirname(os.path.dirname(in_file)),
+                 "sample_silenced", os.path.basename(in_file)])
+    options = nr.get_ambient_clips(in_file)
+    remaining_options = []
+    for pair in options:
+        if ((pair[1] - pair[0]) > 110250):
+            remaining_options.append(pair)
+    num_options = len(remaining_options)
+    if num_options == 0:
+        remaining_options = options
+        num_options = len(remaining_options)
+    chosen_one = remaining_options[random.randrange(0, num_options)]
+    original = pydub.AudioSegment.from_wav(in_file)
+    sample_clip = original.get_sample_slice(0, chosen_one[0])
+    sample_clip.append(pydub.AudioSegment.silent(duration=(chosen_one[1] -
+                       chosen_one[0])))
+    sample_clip.append(original.get_sample_slice(chosen_one[1], None))
+    print(''.join(["Exporting ", os.path.basename(os.path.dirname(out_file)),
+          "/", os.path.basename(out_file)]))
+    sample_clip.export(out_file, format="wav")
 
 def main():
-	iu.i_ursi("/Volumes/data/Research/CDB/openSMILE/", "no_beeps", create_sample, None, ['.contains("long")', '.ends_with(".wav")'])
-		
+    print("running!")
+    iu.i_ursi("/Volumes/data/Research/CDB/openSMILE/all_audio_files",
+              "no_beeps", create_sample, None, ['.contains("long")',
+              '.ends_with(".wav")'])
+
 # ============================================================================
 if __name__ == '__main__':
     main()
