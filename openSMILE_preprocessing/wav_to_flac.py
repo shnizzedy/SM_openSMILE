820f1dff4355ac8c4e6ae1c5f9c861050ce7a888
Jon Clucas <jon.clucas@childmind.org>
2017-01-04 10:26:26 -0500

    to flac

--- openSMILE_preprocessing/wav_to_flac.py
+++ openSMILE_preprocessing/wav_to_flac.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-mxf_to_wav.py
+wav_to_flac.py
 
-Script to quickly convert an mp3 file to a waveform file.
+Script to quickly convert an wav file to a flac file.
 
 Author:
         – Jon Clucas, 2016 (jon.clucas@childmind.org)
@@ -17,27 +17,27 @@ Created on Fri Dec 23 12:43:40 2016
 import argparse, subprocess
 from os import path
 
-def mxf_to_wav(in_file):
+
+def wav_to_flac(in_file):
     # make an output filename
-    out_base = path.basename(in_file).strip('.mxf').strip('.MXF')
+    out_base = path.basename(in_file.strip('.wav').strip('.WAV'))
     out_i = 0
-    out_file = path.join(path.dirname(in_file), ''.join([out_base, '.wav']))
+    out_file = path.join(path.dirname(in_file), ''.join([out_base, '.flac']))
     while path.exists(out_file):
         out_file = path.join(path.dirname(in_file), ''.join([out_base, '_',
-                   str(out_i), '.wav']))
+                   str(out_i), '.flac']))
         out_i = out_i + 1
     # do the conversion verbosely
-    to_convert = ''.join(["ffmpeg -i ", in_file, " -ac 2 -acodec pcm_s32le ",
-                 out_file])
+    to_convert = ''.join(["ffmpeg -i ", in_file, " -c:a flac  ", out_file])
     print(''.join(["Converting ", in_file, " to ", out_file]))
     subprocess.call(to_convert, shell = True)
 
 def main():
     # script can be run from the command line
-    parser = argparse.ArgumentParser(description='get mxf')
+    parser = argparse.ArgumentParser(description='get wav')
     parser.add_argument('in_file', metavar='in_file', type=str)
     arg = parser.parse_args()
-    mxf_to_wav(arg.in_file)
+    wav_to_flac(arg.in_file)
 
 # ============================================================================
 if __name__ == '__main__':
