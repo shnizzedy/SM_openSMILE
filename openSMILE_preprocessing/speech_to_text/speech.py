64a03b282f64e26827e4869faa651e746f638957
Jon Clucas <jon.clucas@childmind.org>
2017-01-04 11:24:18 -0500

    tokenize

--- openSMILE_preprocessing/speech_to_text/speech.py
+++ openSMILE_preprocessing/speech_to_text/speech.py
@@ -2,12 +2,35 @@
 # -*- coding: utf-8 -*-
 """
 """
-import urllib2
-url = "https://www.google.com/speech-api/v1/recognize?"
-      "xjerr=1&client=chromium&lang=en-US"
-audio = open('rainspain.flac','rb').read()
-headers={'Content-Type': 'audio/x-flac; rate=16000',
-         'User-Agent':'Mozilla/5.0'}
-request = urllib2.Request(url, data=audio, headers=headers)
-response = urllib2.urlopen(request)
-print response.read()
+import os, subprocess
+
+def google_speech(in_file):
+    url = ("https://www.google.com/speech-api/v1/recognize?"
+          "xjerr=1&client=chromium&lang=en-US")
+    out_base = os.path.basename(in_file.strip('.flac').strip('.FLAC'))
+    out_file = os.path.join(os.path.dirname(in_file), ''.join([out_base,
+               '.txt']))
+    out_i = 0
+    while os.path.exists(out_file):
+        out_file = os.path.join(os.path.dirname(in_file), ''.join([out_base,
+                   '_', str(out_i), '.txt']))
+        out_i = out_i + 1
+    if not os.path.exists(os.path.dirname(out_file)):
+        os.makedirs(out_file)
+    command_string = ''.join(["curl -X POST --data-binary @", in_file,
+                     " --user-agent 'Mozilla/5.0' --header 'Content-Type:"
+                     " audio/x-flac; rate=16000;' '", url, "'"])
+    subprocess.call(command_string, shell=True)
+    """
+    f = open(out_file, 'w')
+    print(request.data.decode("utf-8", 'ignore'))
+    f.write(request.data.decode("utf-8", 'ignore'))
+    """
+
+def main():
+    google_speech("/Users/jon.clucas/Documents/recorder_test/"
+                  "A001C009_170103PY_CANON.flac")
+
+# ============================================================================
+if __name__ == '__main__':
+    main()
