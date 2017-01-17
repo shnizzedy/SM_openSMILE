dbd8d7c91e167cb9f8744c06c27a698b246906fb
Jon Clucas <jon.clucas@childmind.org>
2017-01-17 12:43:23 -0500

    relative import

--- noise_replacement/nr_openSMILE.py
+++ noise_replacement/nr_openSMILE.py
@@ -23,7 +23,7 @@ Institute, Apache v2.0 License
 
 """
 
-from ... import openSMILE_runSM.mhealthx.mhealthx.extract as ex
+from ...openSMILE_runSM import mhealthx.mhealthx.extract as ex
 from ... import iterate_ursis as iu
 import os
 
